9ed8e1bccaa4d97e47f18f42db548e1a65c6501a
iceBear67 <icebear67@sfclub.cc>
2022-04-17 12:13:33 +0800

    Add IRepositoryResolver

--- api/src/main/java/org/inlambda/packy/manager/IPackageManager.java
+++ api/src/main/java/org/inlambda/packy/manager/IPackageManager.java
@@ -20,59 +20,13 @@
 
 package org.inlambda.packy.manager;
 
-import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
-import org.inlambda.packy.pack.PackageRecord;
-import org.inlambda.packy.pack.metadata.VersionMeta;
-import org.inlambda.packy.repo.sync.IRepositoryClient;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * A service that provides control of package installation and repository synchronization.
  */
 @ApiStatus.AvailableSince("0.1.0")
-public interface IPackageManager {
-    /**
-     * Synchronizes the local repository with the remote repository.
-     */
-    Multi<PackageRecord> fullSync();
+public interface IPackageManager extends IRepositoryResolver {
 
-    /**
-     * Find a package record in all remote repositories.
-     *
-     * @param name    name of package
-     * @param version version of package
-     * @return a {@link Uni} of {@link PackageRecord}s
-     */
-    Uni<PackageRecord> findRemoteRecord(String name, @Nullable String version);
-
-    /**
-     * Synchronizes the local repository with the remote repository for specified record.
-     * This also performs dependency solution if necessary.
-     *
-     * @param targetRepository remote
-     * @param name             name of the package
-     * @param version          version selector, can be empty or null
-     * @return a {@link Uni} of local {@link PackageRecord} or failure
-     */
-    Uni<PackageRecord> syncRecord(IRepositoryClient targetRepository, String name, @Nullable String version);
-
-    /**
-     * Finds required (uninstalled) packages in remote repositories.
-     *
-     * @param record package record
-     * @return a {@link Uni} of {@link PackageRecord}s
-     */
-    Multi<PackageRecord> solveDependency(VersionMeta record);
-
-    /**
-     * Resolves a remote package record.
-     * This will download the package from remote repository and turn into local one.
-     *
-     * @param record remote record
-     * @return a {@link Uni} of local {@link PackageRecord} or failure
-     */
-    Uni<PackageRecord> resolveRecord(PackageRecord record);
 
 }
